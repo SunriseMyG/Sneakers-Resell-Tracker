99e6ee6556a486d31a8248e28b8448581ec9db67
Lucka <lucka.valtriani@epitech.eu>
2024-11-10 06:12:18 +0100

    [ADD] start monitoring for Nocta products in Shopify thread

--- back/main.py
+++ back/main.py
@@ -26,8 +26,12 @@ def start_monitoring():
     corteiz_thread = threading.Thread(target=monitor_shopify, args=('https://www.crtz.xyz/products.json',), daemon=True)
     corteiz_thread.start()
 
+    nocta_thread = threading.Thread(target=monitor_shopify, args=('https://www.nocta.com/products.json',), daemon=True)
+    nocta_thread.start()
+
     snkrs_thread.join()
     corteiz_thread.join()
+    nocta_thread.join()
 
 @app.on_event("startup")
 def startup_event():
