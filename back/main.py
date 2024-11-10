bdf77f9a25a7f9f0cee8f4f3c12e381a86763922
Lucka <lucka.valtriani@epitech.eu>
2024-11-10 06:03:46 +0100

    [ADD] start SNKRS monitoring on FastAPI startup

--- back/main.py
+++ back/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from middlewares.middleware import setupMiddlewares
-
 from routes.default import router as defaultRouter
 from routes.corteiz import router as corteizRouter
 from routes.nike import router as nikeRouter
 from routes.adidas import router as adidasRouter
 from routes.nocta import router as noctaRouter
+from monitors.snkrs import monitor_snkrs
+import threading
 
 app = FastAPI()
 
@@ -16,6 +17,14 @@ app.include_router(corteizRouter)
 app.include_router(nikeRouter)
 app.include_router(adidasRouter)
 app.include_router(noctaRouter)
+
+def start_monitoring():
+    monitoring_thread = threading.Thread(target=monitor_snkrs, daemon=True)
+    monitoring_thread.start()
+
+@app.on_event("startup")
+def startup_event():
+    start_monitoring()
 
 if __name__ == "__main__":
     import uvicorn
