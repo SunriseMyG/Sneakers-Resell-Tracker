18f315e63d2bc6b3328665feb62da19d2f450a1a
Lucka <lucka.valtriani@epitech.eu>
2024-11-10 06:10:59 +0100

    [FIX] start SNKRS monitoring thread and ensure both threads complete

--- back/main.py
+++ back/main.py
@@ -20,10 +20,14 @@ app.include_router(adidasRouter)
 app.include_router(noctaRouter)
 
 def start_monitoring():
-    # snkrs_thread = threading.Thread(target=monitor_snkrs, daemon=True)
-    # snkrs_thread.start()
+    snkrs_thread = threading.Thread(target=monitor_snkrs, daemon=True)
+    snkrs_thread.start()
+    
     corteiz_thread = threading.Thread(target=monitor_shopify, args=('https://www.crtz.xyz/products.json',), daemon=True)
     corteiz_thread.start()
+
+    snkrs_thread.join()
+    corteiz_thread.join()
 
 @app.on_event("startup")
 def startup_event():
