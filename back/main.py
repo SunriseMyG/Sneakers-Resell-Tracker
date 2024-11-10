18de616c1726e7b4aef4a377bd9d7c0e69ac5ac9
Lucka <lucka.valtriani@epitech.eu>
2024-11-10 05:41:42 +0100

    [REFactor] integrate middleware setup and include additional API routers for Corteiz, Nike, Adidas, and Nocta

--- back/main.py
+++ back/main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-import middleware
+from middlewares.middleware import setupMiddlewares
+
+from routes.corteiz import router as corteizRouter
+from routes.nike import router as nikeRouter
+from routes.adidas import router as adidasRouter
+from routes.nocta import router as noctaRouter
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+setupMiddlewares(app)
+
+app.include_router(corteizRouter)
+app.include_router(nikeRouter)
+app.include_router(adidasRouter)
+app.include_router(noctaRouter)
 
 if __name__ == "__main__":
     import uvicorn
