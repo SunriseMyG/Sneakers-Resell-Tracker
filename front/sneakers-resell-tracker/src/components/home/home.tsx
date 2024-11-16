99109f0b2c5d8d07b456535f4f1c60ff86a571ae
StevenDeff <steven.deffontaine@epitech.eu>
2024-11-16 16:05:31 +0100

    [MODIFY] update Home component to include SKU and color, and implement search filtering

--- front/sneakers-resell-tracker/src/components/home/home.tsx
+++ front/sneakers-resell-tracker/src/components/home/home.tsx
@@ -4,138 +4,149 @@ import Travis from './../../images/sneakers.png'
 
 interface HomeProps {
   setPageIndex: React.Dispatch<React.SetStateAction<number>>;
+  setScu: React.Dispatch<React.SetStateAction<string>>;
+  searchItem: string;
 }
 
 let sneakers = [
   {
     id: 1,
-    name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    name: "Caca",
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-1111",
+    color: "Brown",
   },
   {
     id: 2,
-    name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1100,
-    priceOnStockX: 1000,
-    priceOnSecondSteps: 1050,
+    name: "Caneton",
+    price: 1100,
     size: 9,
     img: Travis,
     state: "Low Damaged",
     releaseDate: "2021-05-29",
     sold: true,
+    codeSku: "SKU-2222",
+    color: "Brown",
   },
   {
     id: 3,
-    name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    name: "Sunrise",
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: true,
+    codeSku: "SKU-3333",
+    color: "Brown",
   },
   {
     id: 4,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-4444",
+    color: "Brown",
   },
   {
     id: 5,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-5555",
+    color: "Brown",
   },
   {
     id: 6,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-6666",
+    color: "Brown",
   },
   {
     id: 7,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-7777",
+    color: "Brown",
   },
   {
     id: 8,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-8888",
+    color: "Brown",
   },
   {
     id: 9,
     name: "Jordan 1 Retro High Travis Scott",
-    priceOnWeTheNew: 1500,
-    priceOnStockX: 1200,
-    priceOnSecondSteps: 1400,
+    price: 1500,
     size: 9,
     img: Travis,
     state: "New",
     releaseDate: "2021-05-29",
     sold: false,
+    codeSku: "SKU-9999",
+    color: "Brown",
   },
 ];
 
-function Home({ setPageIndex }: HomeProps) {
+function Home({ setPageIndex, setScu, searchItem }: HomeProps) {
+
+  const handleScu = (scu: string) => {
+    setScu(scu);
+    // console.log(scu);
+    setPageIndex(1);
+  };
+
+  const filteredSneakers = sneakers.filter((sneaker) =>
+    sneaker.name.toLowerCase().includes(searchItem.toLowerCase())
+  );
+
   return (
     <div className="home-container">
       <div className="last-release-container">
 
       </div>
       <div className="sneaker-list-container">
-        {sneakers.map((sneaker) => (
+        {filteredSneakers.map((sneaker) => (
           <div key={sneaker.id} className="sneaker-card">
-            <div className="sneaker-card-info">
+            <div className="sneaker-card-info" onClick={() => { handleScu(sneaker.codeSku) }}>
               <img src={sneaker.img} alt="sneaker" />
               <h2>{sneaker.name}</h2>
-              <p>Price on WeTheNew: {sneaker.priceOnWeTheNew}$</p>
-              <p>Price on StockX: {sneaker.priceOnStockX}$</p>
-              <p>Price on Second Steps: {sneaker.priceOnSecondSteps}$</p>
+              <p>Price: {sneaker.price}$</p>
               <p>Size: {sneaker.size}</p>
-              <p>Condition: {sneaker.state}</p>
               <p>Release Date: {sneaker.releaseDate}</p>
               <p>Sold: {sneaker.sold ? "Yes" : "No"}</p>
+              <p>Color: {sneaker.color}</p>
             </div>
           </div>
         ))}
