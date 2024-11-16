6b2419d62b9d1a08a9df949edfc16749ca2d35dc
StevenDeff <steven.deffontaine@epitech.eu>
2024-11-16 16:03:37 +0100

    [ADD] integrate search functionality and manage state for item display

--- front/sneakers-resell-tracker/src/App.tsx
+++ front/sneakers-resell-tracker/src/App.tsx
@@ -3,17 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/home';
 import Header from './components/header/header';
+import Items from './components/items/items';
 
 function App() {
 
   const [pageindex, setPageIndex] = React.useState(0);
+  const [scu, setScu] = React.useState('');
+  const [searchItem, setSearchItem] = React.useState("");
 
   return (
     <div className="App">
-      <Header setPageIndex={setPageIndex} />
+      <Header
+        setPageIndex={setPageIndex}
+        setSearchItem={setSearchItem}
+      />
       {pageindex === 0 && (
-        <Home setPageIndex={setPageIndex} />
+        <Home
+          setPageIndex={setPageIndex}
+          setScu={setScu}
+          searchItem={searchItem}
+        />
       )}
+      {pageindex === 1 && (
+        <Items
+          setPageIndex={setPageIndex}
+          scu={scu}
+        />
+      )}
+      
     </div>
   );
 }
