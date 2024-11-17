55119e898ae6238a8153a60fc14b388384461399
StevenDeff <steven.deffontaine@epitech.eu>
2024-11-17 22:36:02 +0100

    [MODIFY] implement hamburger menu toggle and conditional search bar display in header component

--- front/sneakers-resell-tracker/src/components/header/header.tsx
+++ front/sneakers-resell-tracker/src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import { CiHeart } from "react-icons/ci";
+import { useState } from "react";
 
 interface HeaderProps {
     setPageIndex: React.Dispatch<React.SetStateAction<number>>;
@@ -9,22 +10,37 @@ interface HeaderProps {
 
 function Header({ setPageIndex, setSearchItem }: HeaderProps) {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchItem(event.target.value);
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <div className="header-container">
             <div className="header-logo" onClick={() => setPageIndex(0)}>
                 Resell Tracker
             </div>
-            <div className="header-brand">
+            <div className={`header-brand ${isMenuOpen ? "active" : ""}`}>
+                {isMenuOpen && (
+                    <div className="header-searchbar-hamburger">
+                        <input type="text" placeholder="Search" onChange={handleSearchChange} />
+                    </div>
+                )}
                 <button>Nike</button>
                 <button>Adidas</button>
                 <button>Jordan</button>
                 <button>Yeezy</button>
                 <button>Supreme</button>
+            </div>
+            <div className="hamburger-menu" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
             <div className="header-searchbar-like">
                 <div className="header-searchbar">
