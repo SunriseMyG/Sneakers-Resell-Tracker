75e9460ba4ce4a929ec00ff33c79bf4a15d801bd
187lucka <lucka.valtriani@epitech.eu>
2024-11-20 14:48:56 +0100

    [MODIFY] enhance read_all endpoint to support SKU filtering and improve query logic

--- back/routes/all.py
+++ back/routes/all.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from sql.connection import getDataBaseConnection
 
 router = APIRouter()
 
 @router.get("/api/all")
-async def read_all(sku: str):
+async def read_all(sku: str = Query(None)):
     with getDataBaseConnection() as (cnx, cursor):
-        query = "SELECT * FROM items"
-        cursor.execute(query)
+        if sku:
+            query = "SELECT * FROM items WHERE sku = %s"
+            cursor.execute(query, (sku,))
+        else:
+            query = "SELECT * FROM items"
+            cursor.execute(query)
+        
         result = cursor.fetchall()
 
         if not result:
