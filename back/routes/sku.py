8a146899388383553574d93a327e1cf24717b335
SunriseMyG <steven.deffontaine@epitech.eu>
2024-11-27 11:35:09 +0100

    [MODIFY] update read_sku endpoint to return specific item fields and improve response structure

--- back/routes/sku.py
+++ back/routes/sku.py
@@ -6,11 +6,20 @@ router = APIRouter()
 @router.get("/api/sku/{sku}")
 async def read_sku(sku: str):
     with getDataBaseConnection() as (cnx, cursor):
-        query = "SELECT * FROM items WHERE sku = %s"
+        query = "SELECT id, name, sku, color, price, image FROM items WHERE sku = %s"
         cursor.execute(query, (sku,))
         result = cursor.fetchall()
 
         if not result:
-            raise HTTPException(status_code=404, detail="SKU {sku} not found")            
+            raise HTTPException(status_code=404, detail=f"SKU {sku} not found")
 
-        return result[0]
+        item = {
+            "id": result[0][0],
+            "name": result[0][1],
+            "sku": result[0][2],
+            "color": result[0][3],
+            "price": result[0][4],
+            "image": result[0][5]
+        }
+
+        return item
